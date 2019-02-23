9ca86d654bf1eef1e76efbed04f3ac69ca67dc3a
Mathias Soeken <mathias.soeken@epfl.ch>
2019-02-23 15:08:18 +0100

    Modified expression.

--- examples/boolean_expression.py
+++ examples/boolean_expression.py
@@ -3,7 +3,7 @@ from qiskit.aqua.algorithms import Grover
 from qiskit_addon_revkit.aqua import BooleanExpression
 
 backend = BasicAer.get_backend('qasm_simulator')
-oracle = BooleanExpression("[[abc]<abc>]")
+oracle = BooleanExpression("[![abc]<abc>]")
 algorithm = Grover(oracle)
 result = algorithm.run(backend)
 print(result['result'])
